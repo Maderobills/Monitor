a2e079e56073e01dd5aa87c46c3b289925697a17
Maderobills <96554416+Maderobills@users.noreply.github.com>
2023-02-03 01:48:57 +0000

    Initial commit

---
+++ app/src/main/java/com/example/monitor/Registration.java
@@ -0,0 +1,93 @@
+package com.example.monitor;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Registration extends AppCompatActivity {
+
+    private EditText mEmail, mPass;
+    private Button btnReg;
+    private TextView mLoginReg;
+
+    private ProgressDialog mDialog;
+
+    //Fire
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mDialog = new ProgressDialog(this);
+
+        registration();
+    }
+
+    private void registration(){
+
+        mEmail = findViewById(R.id.email_reg);
+        mPass = findViewById(R.id.password_reg);
+        btnReg = findViewById(R.id.btn_reg);
+        mLoginReg = findViewById(R.id.login_reg);
+
+        btnReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String email = mEmail.getText().toString().trim();
+                String pass = mPass.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)){
+                    mEmail.setError("Email Required...");
+                    return;
+                }
+                if (TextUtils.isEmpty(pass)){
+                    mPass.setError("Password Required...");
+                    return;
+                }
+
+                mAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if (task.isSuccessful()){
+
+                            Toast.makeText(Registration.this, "Registration complete", Toast.LENGTH_SHORT).show();
+
+                        }else {
+                            Toast.makeText(Registration.this, "Registration Failed...!", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+
+            }
+        });
+
+        mLoginReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            }
+        });
+
+    }
+}
